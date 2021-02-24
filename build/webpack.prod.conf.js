d02cb71d37165931299c69bc83c492ebd922047d
JacobLinCool <jacoblincool@gmail.com>
2021-02-25 00:21:55 +0800

    add google search console verification file

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,18 +1,18 @@
-'use strict'
-const path = require('path')
-const utils = require('./utils')
-const webpack = require('webpack')
-const config = require('../config')
-const merge = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const PreloadWebpackPlugin = require('@vue/preload-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-const TerserPlugin = require("terser-webpack-plugin")
+"use strict";
+const path = require("path");
+const utils = require("./utils");
+const webpack = require("webpack");
+const config = require("../config");
+const merge = require("webpack-merge");
+const baseWebpackConfig = require("./webpack.base.conf");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const PreloadWebpackPlugin = require("@vue/preload-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
-const env = require('../config/prod.env')
+const env = require("../config/prod.env");
 
 const webpackConfig = merge(baseWebpackConfig, {
     mode: "production",
@@ -20,47 +20,47 @@ const webpackConfig = merge(baseWebpackConfig, {
         rules: utils.styleLoaders({
             sourceMap: config.build.productionSourceMap,
             extract: true,
-            usePostCSS: true
-        })
+            usePostCSS: true,
+        }),
     },
     devtool: config.build.productionSourceMap ? config.build.devtool : false,
     output: {
         path: config.build.assetsRoot,
-        filename: utils.assetsPath('js/[name].[chunkhash].js'),
-        chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+        filename: utils.assetsPath("js/[name].[chunkhash].js"),
+        chunkFilename: utils.assetsPath("js/[id].[chunkhash].js"),
     },
     plugins: [
         // http://vuejs.github.io/vue-loader/en/workflow/production.html
         new webpack.DefinePlugin({
-            'process.env': env
+            "process.env": env,
         }),
         new MiniCssExtractPlugin({
-            filename: utils.assetsPath('css/[name].[contenthash].css'),
-            ignoreOrder: true
+            filename: utils.assetsPath("css/[name].[contenthash].css"),
+            ignoreOrder: true,
         }),
         // Compress extracted CSS. We are using this plugin so that possible
         // duplicated CSS from different components can be deduped.
         new OptimizeCSSPlugin({
             cssProcessorOptions: config.build.productionSourceMap
-            ? { safe: true, map: { inline: false } }
-            : { safe: true }
+                ? { safe: true, map: { inline: false } }
+                : { safe: true },
         }),
         // generate dist index.html with correct asset hash for caching.
         // you can customize output by editing /index.html
         // see https://github.com/ampedandwired/html-webpack-plugin
         new HtmlWebpackPlugin({
             filename: config.build.index,
-            template: 'index.html',
+            template: "index.html",
             inject: true,
             minify: {
                 removeComments: true,
                 collapseWhitespace: true,
                 removeAttributeQuotes: true,
                 sortAttributes: true,
-                sortClassName: true
+                sortClassName: true,
                 // more options:
                 // https://github.com/kangax/html-minifier#options-quick-reference
-            }
+            },
         }),
         new PreloadWebpackPlugin(),
         // keep module.id stable when vendor modules does not change
@@ -70,39 +70,39 @@ const webpackConfig = merge(baseWebpackConfig, {
         new CopyWebpackPlugin({
             patterns: [
                 {
-                    from: path.resolve(__dirname, '../static'),
+                    from: path.resolve(__dirname, "../static"),
                     to: config.build.assetsSubDirectory,
                     globOptions: {
-                        ignore: ['.*']
-                    }
-                }
-            ]
-        })
+                        ignore: [".*"],
+                    },
+                },
+                {
+                    from: path.resolve(__dirname, "../GOOGLE_SEARCH_CONSOLE"),
+                    to: path.resolve(__dirname, "../dist"),
+                },
+            ],
+        }),
     ],
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin()],
-        concatenateModules: true
-    }
-})
+        concatenateModules: true,
+    },
+});
 
 if (config.build.productionGzip) {
-    const CompressionWebpackPlugin = require('compression-webpack-plugin')
+    const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
     webpackConfig.plugins.push(
         new CompressionWebpackPlugin({
-            test: new RegExp(
-                '\\.(' +
-                config.build.productionGzipExtensions.join('|') +
-                ')$'
-            )
+            test: new RegExp("\\.(" + config.build.productionGzipExtensions.join("|") + ")$"),
         })
-    )
+    );
 }
 
 if (config.build.bundleAnalyzerReport) {
-    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-    webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+    const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+    webpackConfig.plugins.push(new BundleAnalyzerPlugin());
 }
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
